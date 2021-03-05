e1ce9777e3f1a879d6e5316db7da364cb05bd142
Steve Eckles <steve.d.eckles@gmail.com>
2021-03-05 11:53:58 -0500

    Add test example for pg connection

---
+++ pages/test.js
@@ -0,0 +1,27 @@
+const db = require('../db')
+
+export async function getStaticProps () {
+  const query = {
+    text: 'SELECT * FROM users',
+  }
+  try {
+    const res = await db.query(query)
+    const userData = res.rows[0]
+    const otherUser = res.rows[1]
+    return {
+      props: {
+        userData,
+        otherUser
+      }
+    }
+  } catch (err) {
+    console.log(err.stack)
+  }
+
+}
+
+export default function Test({ userData, otherUser }) {
+  return (
+    <p>{userData.email} + {otherUser.email}</p>
+  )
+}
