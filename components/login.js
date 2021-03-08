9b5259185e11d9e32aef3af7d27e72937081a860
Kate Mundale <kdmundale192@stevenscollege.edu>
2021-03-08 10:51:36 -0500

    typo fix on htmlFor

--- components/login.js
+++ components/login.js
@@ -1,7 +1,7 @@
 export default function LoginForm(){
     return(
       <form>
-        <label htmfor="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input id="email" type="text" autoComplete="email" required />
         <button type="submit">Login</button>
       </form>
