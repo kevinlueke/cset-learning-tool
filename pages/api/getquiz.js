2443d44f258c76b86f203ed1461d9857ca19c46a
Kate Mundale <kdmundale192@stevenscollege.edu>
2021-05-04 10:57:31 -0400

    added api to fetch quiz data for each quiz

--- pages/api/getquiz.js
+++ pages/api/getquiz.js
@@ -2,12 +2,14 @@ import withSession from '../../lib/session'
 
 export default withSession(async (req, res) => {
 
-  const { qid , no_questions } = await req.body
+  const { cid , no_ques } = await req.body
   const db = require('../../db')
 
+  console.log(cid, no_ques, "!!!!!!!!!!!!!!!!!!")
+
   const query = {
-    text: "SELECT q.question,q.clue,q.ans,q.res_a,q.res_b,q.res_c,q.res_d,q.id,con.title FROM questions q JOIN concepts con ON q.concept_id = con.id WHERE q.concept_id = $1 ORDER BY RAND() LIMIT $2",
-    values:[qid,no_questions],
+    text: "SELECT q.question,q.clue,q.ans,q.res_a,q.res_b,q.res_c,q.res_d,q.id,con.title FROM questions q JOIN concepts con ON q.concept_id = con.id WHERE q.concept_id = $1 ORDER BY random() LIMIT $2",
+    values:[cid,no_ques],
   }
 
   try {
