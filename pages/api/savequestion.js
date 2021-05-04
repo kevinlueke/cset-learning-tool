232c319dc4018f1937e8c49f985dc29024646c96
Kate Mundale <kdmundale192@stevenscollege.edu>
2021-05-04 11:03:09 -0400

    api to save question results as student answers

--- pages/api/savequestion.js
+++ pages/api/savequestion.js
@@ -4,8 +4,6 @@ export default withSession(async (req, res) => {
   const {student_id, question_id, question_result, save} = await req.body
   const db = require('../../db')
 
-  console.log(student_id,question_id, question_result, save)
-
   const query = {
     text:"INSERT INTO student_questions (student_id, question_id, question_result, save) VALUES ($1, $2,$3,$4) ON CONFLICT (student_id,question_id) DO UPDATE SET question_result=$3, save = $4",
     values:[student_id,question_id, question_result, save],
@@ -13,9 +11,14 @@ export default withSession(async (req, res) => {
 
   try {
     const db_res = await db.query(query)
-    res.status(200).json(db_res.data)
+    res.status(200).json({
+      type: 'saved',
+      message: 'question response saved'
+    })
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error.stack)
+    res.status(400).json({
+      type: 'error',
+      message: 'result not saved'
+    })
   }
 })
